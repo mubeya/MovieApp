3fbd7e70bd00cac95d1d2e67ccac445458ae14d8
mubeya <mubeyafirat@gmail.com>
2021-06-17 22:59:23 +0300

    Second Commit

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const APIURL = config.APIURL;
 const GENREAPIURL = config.GENREAPIURL;
 const IMGPATH = config.IMGPATH;
 const SEARCHAPI = config.SEARCHAPI;
+const TOPRATEDURL = config.TOPRATEDURL;
+const UPCOMINGURL = config.UPCOMINGURL;
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
@@ -24,57 +26,75 @@ async function getMovies(url){
 function showMovies(movies){
   
     main.innerHTML =``;  //clear main area
-    
     movies.forEach((movie) => {   
-   
-        const {poster_path, title, vote_average, overview, genre_ids} = movie;
+        const {id, poster_path, title, vote_average, overview, genre_ids} = movie;
         const  movieEl = document.createElement("div");    //create "movie" class div in main area
         movieEl.classList.add("movie");
-        
+
         fetch(GENREAPIURL).then(res => res.json()).then((genreData) => {   //get movie genre from API
             const movieGenre1 = genreData.genres.find(item => item.id === genre_ids[0]);
             const movieGenre2 = genreData.genres.find(item => item.id === genre_ids[1]);
 
+            const TRAILERURL = "https://api.themoviedb.org/3/movie/"+id+"/videos?api_key=" + APIKEY;  // get movie trailer from another API
+            fetch(TRAILERURL).then(res => res.json()).then(res => {let youtube = "https://www.youtube.com/embed/" + res.results[0].key;
+
             movieEl.innerHTML = `
+            <span class="movie-vote"><i class="fa fa-star"></i>${vote_average}</span>
             <img 
                 class="image"
                 src="${IMGPATH + poster_path}" 
                 alt="${title}" />
             <div class="movie-info">
-            <div><h3>${title}</h3></div>
+            <div><p class="title">${title}</p></div>
             <div>
-                <span>IMDB:${vote_average}</span>
                 <span class="detail">Details</span>
-                <p class="genre">${movieGenre1.name},${movieGenre2.name}</p>
+                <button id="${id}" class="similar" onclick="similarMovies(this.id)">Similar Movies</button>
+                <p class="genre">${movieGenre1.name} / ${movieGenre2.name}</p>
             </div> 
             </div> 
             <div class="modal-overlay">
             <div class="modal-container">
                 <h3>Overview</h3>
-                <p>${overview}</p>
+                <p style="margin: 20px auto;">${overview}</p>
+                <h4>Trailer</h4>
+                <iframe width="420" height="315" src="${youtube}"></iframe>
                 <button class="close-btn"><i class="fas fa-times"></i></button>
             </div>
             </div>`;
             
             main.appendChild(movieEl);
           
-            const detailBtns = document.querySelectorAll(".detail");     
+            const detailBtns = document.querySelectorAll(".detail");   
+            const similarBtns = document.querySelectorAll(".similar");     
             const modals = document.querySelectorAll(".modal-overlay");
             const closeBtns = document.querySelectorAll(".close-btn");
                   
             
-            detailBtns.forEach(function(detailBtn,index){                // open modal popup for overview
-                detailBtn.addEventListener("click", function(){
-                    modals[index].classList.add("open-modal");
-                }); 
-        
-                closeBtns[index].addEventListener("click", function(){
-                modals[index].classList.remove("open-modal");  
-                });  
-        
+            detailBtns.forEach(function(detailBtn,index){         // open modal popup for overview
+               detailBtn.addEventListener("click", function(){
+                   modals[index].classList.add("open-modal");
+               }); 
+       
+               closeBtns[index].addEventListener("click", function(){
+               modals[index].classList.remove("open-modal");  
+               });  
             });
-        });        
+         }); 
+       });        
     });
+}
+
+function similarMovies(id){
+    const SIMILARURL =  "https://api.themoviedb.org/3/movie/"+ id +"/similar?api_key="+APIKEY;
+    getMovies(SIMILARURL);
+}
+
+function topRatedMovies(){
+    getMovies(TOPRATEDURL);
+}
+
+function upcomingMovies(){
+    getMovies(UPCOMINGURL);
 }
 
 form.addEventListener("submit", (e) =>{
@@ -87,7 +107,6 @@ form.addEventListener("submit", (e) =>{
         search.value = "";
     }
 });
-
 
 let pageCount = 1;
 nextBtn.addEventListener("click", function(){     //move to next page
